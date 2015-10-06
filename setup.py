5d6de821df7047854c49ac3e8ebf01678309da30
Necdet Can Atesman <c.atesman@gmx.net>
2015-10-06 15:15:33 +0200

    setup.py now pointing to correct README file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 setup(
