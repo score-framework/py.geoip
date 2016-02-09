6fabdfdc2a8bd9d738b0f08d7261cb08a942cde0
Necdet Can Atesman <c.atesman@gmx.net>
2016-02-09 10:20:41 +0100

    Release: 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.geoip',
-    version='0.2',
+    version='0.2.1',
     description='IP Geolocalization for The SCORE Framework',
     long_description=README,
     author='strg.at',
